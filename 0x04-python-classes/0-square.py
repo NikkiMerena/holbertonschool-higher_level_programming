8d058cf33d37cd74f5bb86adb0622435e6170828
NikkiMerena <alderman.nikki1022@gmail.com>
2023-01-18 08:13:01 -0800

    doc updated

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
-"""This is the square class!"""
+"""
+
+This is the square class!
+
+"""
 
 
 class Square:
     """This class defines a square"""
     def __init__(self, size=None):
         """This initializes a square with a size"""
-       self.__size = size
+        self.__size = size
