890a98d3ae87d77266284a5c31e46357f1bb0b87
NikkiMerena <alderman.nikki1022@gmail.com>
2023-01-19 18:14:25 +0000

    task 4

---
+++ 0x04-python-classes/4-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""Creating class square with exceptions"""
+
+
+class Square:
+    """This is the Square"""
+    def __init__(self, size=0):
+        """Adding private size attribute with if guards"""
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """This is creating area from size"""
+        return (self.__size**2)
+
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print("")
