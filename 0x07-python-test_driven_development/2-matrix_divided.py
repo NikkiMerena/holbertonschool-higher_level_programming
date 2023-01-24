bc6852ac347454ea5138c892f3103ffd21558bbe
NikkiMerena <alderman.nikki1022@gmail.com>
2023-01-24 14:57:10 -0800

    and again

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,42 +1,25 @@
 #!/usr/bin/python3
-"""This program divides every value in a matrix by a given divisor
-"""
+"""Task 1 - Divide items in a matrix"""
+
+
+def bail():
+    raise TypeError(
+        "matrix must be a matrix (list of lists) of integers/floats")
 
 
 def matrix_divided(matrix, div):
-    """matrix is the provided matrix, div is the divisor
-    The matrix should consist only of ints or floats
-    The divisor should also be an int or float, and not zero
-    Resultant matrix should have values rounded to two places
-    New matrix should be returned
-    Each row of the matrix should be the same size
-    """
-
-    """Check that divisor is not zero"""
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-
-    """Chech that divisor is present"""
-    if div is None:
-        raise TypeError("matrix_divided() missing 1\
-                        required positional argument: 'div'")
-
-    """Check that divisor is a number"""
-    if type(div) is not int and type(div) is not float:
+    """Divide items in matrix by div"""
+    if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-
-    """Check that each row of the matrix is the same size"""
-    row_length = len(matrix[0])
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+    last = None
+    newMatrix = []
     for row in matrix:
-        if len(row) != row_length:
+        if last is not None and last != len(row):
             raise TypeError("Each row of the matrix must have the same size")
-
-    """Check that each value in the matrix is a number"""
-    for row in matrix:
-        for num in row:
-            if type(num) is not int and type(num) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) \
-of integers/floats")
-
-    """Finally, return a new matrix"""
-    return[[round(num/div, 2) for num in row] for row in matrix]
+        newMatrix.append(
+            [round(x/div, 2) if isinstance(x, (int, float)) else bail()
+             for x in row])
+        last = len(row)
+    return newMatrix
