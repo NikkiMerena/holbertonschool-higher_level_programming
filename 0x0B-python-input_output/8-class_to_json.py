55a2cd0147e4fb997bcecd6d522f2a9d24a956d2
NikkiMerena <alderman.nikki1022@gmail.com>
2023-01-31 11:09:22 -0800

    task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Project 0x08. Python - Input/Output
+Task 8
+"""
+
+
+def class_to_json(obj):
+    """function that returns the dictionary description with
+    simple data structure
+    """
+
+    return (vars(obj))
