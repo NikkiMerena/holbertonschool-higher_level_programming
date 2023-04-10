135afcdefdf0348faf23a76724b1680cb1235925
NikkiMerena <alderman.nikki1022@gmail.com>
2023-04-10 09:49:31 -0700

    fic

--- 0x00-javascript-warm_up/7-multi_c.js
+++ 0x00-javascript-warm_up/7-multi_c.js
@@ -6,9 +6,7 @@ const x = parseInt(arg);
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-  let message = '';
   for (let i = 0; i < x; i++) {
-    message += 'C is fun\n';
+    console.log('C is fun');
   }
-  console.log(message.trim());
 }
