c00559a3886a227c2d7bfe5ddabb69322f3aaafe
NikkiMerena <alderman.nikki1022@gmail.com>
2023-04-13 12:18:54 -0700

    updated

--- 0x02-javascript-web_scraping/0-readme.js
+++ 0x02-javascript-web_scraping/0-readme.js
@@ -1,11 +1,15 @@
 #!/usr/bin/node
 
+// Load the fs module for working with the file system
 const fs = require('fs');
 
+// Read the file specified by the user in the command-line argument
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  // If an error occurred while reading the file, log the error to the console
   if (err) {
     console.log(err);
   } else {
+    // If the file was read successfully, log its contents to the console
     console.log(data);
   }
 });
