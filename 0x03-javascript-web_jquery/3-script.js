110ba7babc92448d103749ebaeed8b6f9cf35e50
root <root@LAPTOP-BS5Q7L8B.localdomain>
2023-04-18 14:05:10 -0500

    task3,4,5

---
+++ 0x03-javascript-web_jquery/3-script.js
@@ -0,0 +1,8 @@
+// Wait for the document to be ready
+$(document).ready(function () {
+  // Add click event handler to the element with id "red_header"
+  $('#red_header').click(function () {
+  // Add the "red" class to the header element
+    $('header').addClass('red');
+  });
+});
